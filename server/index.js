ed6dea8c6f2ea97b239923a0b7154f2271d3fe99
David Hong <phazex1@gmail.com>
2021-10-21 18:28:53 -0400

    Updating commits, will work on more features

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { read } = require('fs')
 const path = require('path')
 
 const app = express()
@@ -6,9 +7,25 @@ const app = express()
 app.get('/', function(req, res){
     res.sendFile(path.join(__dirname, "../index.html"))
 })
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname, "../classes.html"))
+})
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname, "../followers.html"))
+})
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname, "../items.html"))
+})
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname, "../weapons.html"))
+})
 
 app.get('/css', function(req, res){
     res.sendFile(path.join(__dirname, "../styles.css"))
+})
+
+app.get('/js', function(req, res){
+    read.sendFile(path.join(__dirname, "../main.js"))
 })
 
 const port = process.env.PORT || 4567
